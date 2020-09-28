5385a7f6dd592ccab4a4394a52869188583ff87f
nxsh <ryan.nash43@gmail.com>
2020-09-28 21:14:31 +0100

    Settings tab now shows coronavirus count of Walsgrave in past 7 days

--- README.md
+++ README.md
@@ -5,8 +5,10 @@ Start page for web browsers with functions like weather and frequent links
 * Cycle search engines: DONE
 * Set name via local storage (pressing enter collides with search function) DONE
 * Isolate keypresses on page from those when in an input DONE
+* Order flex settings (nav) to be last on mobile DONE
+
 * Vertical center for page
 * Store search engine selection as a cookie/local storage
 * Light mode theme toggle using cookies/local storage
 * Viewport break at 641px to 1400px so that columns collapse into two rows
-* Order flex settings (nav) to be last on mobile DONE
+
--- script.js
+++ script.js
@@ -100,6 +100,19 @@ $.getJSON("https://ip-geolocation.whoisxmlapi.com/api/v1?apiKey=at_87UkRbwACHaX1
 	document.getElementById('loc').textContent = data.location.city;
 });
 
+var corona;
+$.getJSON("https://c19downloads.azureedge.net/downloads/msoa_data/MSOAs_latest.json", function(data) {
+	//console.log(data.data[1908].lad19_nm); // Coventry, Walsgrave
+	corona = data.data[1908].latest_7_days;
+	document.getElementById('cc').textContent=corona+" coronavirus cases in Walsgrave in the last 7 days";
+	//JSON.parse(data).data.find(element => element.msoa11_cd === 'E02006525'));
+});
+
+console.log(corona + " cases in Walsgrave, Coventry in the last 7 days");
+
+//JSON.parse(corona).corona.find(element => element.msoa11_cd === 'E02006525');
+
+
 // Google maps
 function initMap() {
 	$.getJSON("https://am.i.mullvad.net/json", function(data){ // redundant need to merge with other call
